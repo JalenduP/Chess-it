5e86a8f4a968428bbc09cf367a5b460a00ef19cf
JalenduP <jalendupandey@gmail.com>
2025-10-19 19:26:26 +0400

    Changed font to aclonia

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        // Add this line
+        'sans': ['Aclonia', 'sans-serif'] // Sets Aclonia as the default sans-serif font
+      },
       colors: {
         // You can add your "chess yellow" here for easy reuse
         'chess-yellow': '#D4DB00', // This is a guess, use your exact color
